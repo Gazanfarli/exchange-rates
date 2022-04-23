c716a9603036f7b066adb58f076cf6d86c421dc3
gazanfarli <qznfrli.2001@gmail.com>
2022-04-23 17:25:33 +0400

    some style and js files update

--- app.js
+++ app.js
@@ -1,15 +1,85 @@
-const url = "https://api.exchangerate.host/latest"
+const exchange_api_url = "https://api.exchangerate.host/latest?base="
 const currencyInput = document.querySelector("#input");
 const currencyResult = document.querySelector("#result");
 
 eventListeners();
+
 function eventListeners(){
+    document.addEventListener("DOMContentLoaded", convertDefault)
     document.addEventListener("click", changeActive);
-    currencyInput.addEventListener("click", convertCurrency)
+    currencyInput.addEventListener("keyup", convertCurrencyFromInput)
+    currencyResult.addEventListener("keyup", convertCurrencyFromResult)
+}
+
+
+function convertCurrencyFromInput(){ // Convert from main input
+    let fromCurrency = document.querySelectorAll(".active")[0].innerText;
+    let toCurrency = document.querySelectorAll(".active")[1].innerText;
+
+    if(isEqual()){
+        currencyResult.value = currencyInput.value;
+        return 0;
+    };
+
+    getRatesFromAPI(exchange_api_url + `${fromCurrency}&symbols=${toCurrency}`)
+    .then(data => {
+        let toValue = Object.values(data)[0];
+        currencyResult.value = (currencyInput.value * toValue).toFixed(4);
+    })
+    .catch(err => console.log(err));
+}
+
+function convertCurrencyFromResult(){ // Convert from Result input
+    let fromCurrency = document.querySelectorAll(".active")[1].innerText;
+    let toCurrency = document.querySelectorAll(".active")[0].innerText;
+
+    if(isEqual()){
+        currencyInput.value = currencyResult.value;
+        return 0;
+    };
+
+    getRatesFromAPI(exchange_api_url + `${fromCurrency}&symbols=${toCurrency}`)
+    .then(data => {
+        let toValue = Object.values(data)[0];
+        currencyInput.value = (currencyResult.value * toValue).toFixed(4);
+    })
+    .catch(err => console.log(err));
+}
+
+function getRatesFromAPI(url){
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data.rates))
+        .catch(() => reject("Someting went wrong... can't fetch data from API"));
+    })
+}
+
+function isEqual(){
+    const actives = [...document.querySelectorAll(".active")];
+    if(actives[0].textContent == actives[1].textContent){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function convertDefault(){
+    let fromCurrency = document.querySelectorAll(".active")[0].innerText;
+    let toCurrency = document.querySelectorAll(".active")[1].innerText;
+
+    getRatesFromAPI(exchange_api_url + `${fromCurrency}&symbols=${toCurrency}`)
+    .then(data => {
+        let toValue = Object.values(data)[0];
+        currencyInput.value = 1;
+        currencyResult.value = toValue.toFixed(4);
+        changeRateText(fromCurrency, toCurrency);
+    })
 }
 
 function changeActive(e){
     if(e.target.className.includes("currency-item")){
+        isEqual();
         const currencies = [...e.target.parentElement.children];
         currencies.forEach(e => {
             if(e.className.indexOf("active") !== -1){
@@ -18,39 +88,22 @@ function changeActive(e){
         })
         e.target.classList.add("active");
     }
+    changeRateText();
+    convertCurrencyFromInput();
 }
 
-function convertCurrency(event){
-    let fromCurrency = document.querySelectorAll(".active")[0].innerText;
-    let fromValue = 0;
-    let rates;
-    getRatesFromAPI()
+function changeRateText(){
+    let fromCurrencyRate = document.querySelector(".input-rate");
+    let toCurrencyRate = document.querySelector(".result-rate");
+    const fromCurrency = document.querySelectorAll(".active")[0].innerText;
+    const toCurrency = document.querySelectorAll(".active")[1].innerText;
+
+    getRatesFromAPI(exchange_api_url + `${fromCurrency}&symbols=${toCurrency}`)
     .then(data => {
-        rates = data;
+        fromCurrencyRate.textContent = `1 ${fromCurrency} = ${Object.values(data)[0].toFixed(4)} ${toCurrency}`;
     })
-    .catch(err => console.log(err));
-    // console.log(rates.);
-    console.log(fromCurrency)
+    getRatesFromAPI(exchange_api_url + `${toCurrency}&symbols=${fromCurrency}`)
+    .then(data => {
+        toCurrencyRate.textContent = `1 ${toCurrency} = ${Object.values(data)[0].toFixed(4)} ${fromCurrency}`
+    })
 }
-
-async function getRatesFromAPI(){
-    let rates =  await fetch(url);
-    const data = await rates.json();
-    return data.rates;
-}
-getRatesFromAPI()
-.then(data => {
-    console.log(data)
-})
-.catch(err => console.log(err));
-
-// function getCurrencyRate(fromCurrency){
-//     let fromValue; 
-//     getRatesFromAPI()
-//     .then(data => {
-//         fromValue = data.fromCurrency;
-//     })
-//     .catch(err => console.log(err))
-//     console.log(fromValue);
-// }
-// getCurrencyRate("RUB");
